23dbcc8ffc68124dfe0be5b8b94bad86a530a47e
Katerina <psyckalla.gmail.com>
2020-09-25 14:57:42 -0400

    context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import RenderFolder from './RenderFolder';
+import RenderNote from './RenderNote';
+import Main from './Main';
+import { FolderProvider } from './FolderContext';
+import { NoteProvider } from './NoteContext';
+import Nav from './Nav';
 
 function App() {
   useEffect(() => {
     fetchData();
   }, []);
 
-const fetchData = async () => {
-  const folders = await fetch('http://localhost:8000/foldersfetch');
-  const folderItems = await folders.json();
+  const [notes, setNotes] = useState({});
+  const [folders, setFolders] = useState({})
 
-  const notes = await fetch('http://localhost:8000/notesfetch');
-  const notesItems = await notes.json();
+const fetchData = async () => {
+  const foldersData = await fetch('http://localhost:8000/foldersfetch');
+  const folderItems = await foldersData.json();
+
+  const notesData = await fetch('http://localhost:8000/notesfetch');
+  const notesItems = await notesData.json();
   console.log(folderItems);
   console.log(notesItems);
+  setNotes(notesItems);
+  setFolders(folderItems);
 }
-
-
 
   return (
     <div className="App">
-     
+      <NoteProvider value={notes}>
+        <FolderProvider value={folders}>
+          <Nav />
+          <Route exact path="/" component={Main} />
+          <Route path="/folder/:id" component={RenderFolder} />
+          <Route path="/note/:id" component={RenderNote} />
+        </FolderProvider>
+
+      </NoteProvider>
+      
     </div>
   );
 }
---
+++ src/FolderContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FolderContext = React.createContext();
+
+export const FolderProvider = FolderContext.Provider;
+
+export default FolderContext;
